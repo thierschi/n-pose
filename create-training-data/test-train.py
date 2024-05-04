eb294be901b35d2b5cda5232eca9643d3e1e6786
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-05-04 13:15:39 +0200

    Minor changes

--- create-training-data/test-train.py
+++ create-training-data/test-train.py
@@ -1,16 +1,22 @@
-from convert_data import *
+import sys
+
 from ultralytics import YOLO
+
+from convert_data import *
+
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+print(os.getcwd())
 
 captures = get_captures_from_data('../data/data')
 labels = get_labels('../data/data')
-yaml_path = create_training_data('./training_data', captures, labels)
-
-
+yaml_path = create_training_data('../data/training_data', captures, labels)
+# yaml_path = 'coco8-seg.yaml'
 
 # Load a model
-model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8m-seg.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data=yaml_path, epochs=1, imgsz=640)
-
-print(results)
+results = model.train(data=yaml_path, epochs=100, imgsz=640, device='cpu', project='../logs')
