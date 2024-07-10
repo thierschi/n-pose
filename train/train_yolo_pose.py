4cfbe37a5b5fbb165c10b1f1c8191946e70c4f36
Lukas Thiersch <lukas.thiersch@uni-bayreuth.de>
2024-07-10 18:35:36 +0200

    Createted PY File for training YOLOPose Models

--- train/train_yolo_pose.py
+++ train/train_yolo_pose.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 from import_data.unity_data import UnityData
 from convert_data.unity_to_yolo_pose import unity_to_yolo_pose
 
-data = UnityData('../data/solo_90')
-yaml_path = unity_to_yolo_pose(data, '../data/solo_90_yolo', 5, [0, 2, 1, 4, 3], include_test=True)
+data = UnityData('data/solo_93')
+yaml_path = unity_to_yolo_pose(data, 'data/solo_93_yolo', 5, [0, 2, 1, 4, 3], include_test=True)
 
-model = YOLO("yolov8x-pose.pt")
+model = YOLO("logs/solo_90/train/weights/best.pt")
 
-results = model.train(data=yaml_path, epochs=100, imgsz=640, device='cuda', project='../logs/solo_90')
+results = model.train(data=yaml_path, epochs=100, imgsz=640, device='cuda', project='logs/solo_93')
