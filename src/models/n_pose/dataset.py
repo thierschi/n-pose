3e9e80c621a9578105ca29a62a0d59db9618449c
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-08-20 14:48:55 +0200

    Adjusted Solo Dataset

--- src/models/n_pose/dataset.py
+++ src/models/n_pose/dataset.py
@@ -1,19 +1,11 @@
-import numpy as np
 import torch
 from torch.utils.data import Dataset
 
 
 class SoloData(Dataset):
     def __init__(self, xy):
-        x = xy[:, 3:]
-        kp_l = x[:, 0: 10]
-        kp_r = x[:, 110: 120]
-
-        joined = np.concatenate((kp_l, kp_r), axis=1)
-
-        self.x = torch.from_numpy(joined)
-
-        self.y = torch.from_numpy(xy[:, 0:3])
+        self.x = torch.from_numpy(xy[:, 6:])
+        self.y = torch.from_numpy(xy[:, :6])
         self.n_samples = xy.shape[0]
 
     def __len__(self):
