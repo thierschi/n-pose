2f7f548f1fc2d2f2083423796c8854f667352fec
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-08-28 16:38:39 +0200

    Added vector norm util to take an opt. target length

--- src/util/vector.py
+++ src/util/vector.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-def normalize_vector(v):
+def normalize_vector(v, length=1):
     v = np.array(v)
-    return v / np.linalg.norm(v)
+    return v / np.linalg.norm(v) * length
