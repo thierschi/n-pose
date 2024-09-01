8fdcfe26a5fb819e57d8bdb45278a26ef2c0ed58
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-09-01 15:08:06 +0200

    Added AOE and ATE metric

---
+++ src/models/n_pose/metrics/aoe.py
@@ -0,0 +1,31 @@
+import torch
+
+
+class AOE:
+    """
+    Average Orientation Error (AOE) metric.
+    """
+    total_error: float
+    count: int
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.total_error = 0.0
+        self.count = 0
+
+    def update(self, predicted, ground_truth):
+        predicted = predicted / torch.norm(predicted, dim=1, keepdim=True)
+        ground_truth = ground_truth / torch.norm(ground_truth, dim=1, keepdim=True)
+
+        dot_product = (predicted * ground_truth).sum(dim=1)
+        angular_error = torch.acos(torch.clamp(dot_product, -1.0, 1.0))
+
+        self.total_error += angular_error.sum().item()
+        self.count += predicted.size(0)
+
+    def compute(self):
+        if self.count == 0:
+            return 0.0
+        return self.total_error / self.count
