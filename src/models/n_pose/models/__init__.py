3f3eb0689e37639ffb71b39c656a8628aff844a1
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-09-04 18:38:38 +0200

    Modified combis

--- src/models/n_pose/models/__init__.py
+++ src/models/n_pose/models/__init__.py
@@ -1,5 +1,6 @@
 from .balanced_fcn import BalancedFCN
 from .compact_fcn import CompactFCN
+from .custom_fcn import CustomFCN
 from .deep_fcn import DeepFCN
 from .simple_fcn import SimpleFCN
 from .small_fcn import SmallFCN
