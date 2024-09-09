f2663d617bc039f3d92e3feac851ee6bf8bff607
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-09-09 14:48:27 +0200

    Adapted yolo training scripts

--- scripts/yolo/train_yolo.py
+++ scripts/yolo/train_yolo.py
@@ -1,8 +1,14 @@
+import os
+
 from ultralytics import YOLO
 
 from src.data.unity_data import UnityData
 from src.data.yolo_conversion import unity_to_yolo_pose, unity_to_yolo_seg
 
+path = os.getcwd()
+while not os.path.exists(os.path.join(path, 'src')):
+    path = os.path.join(path, "..")
+os.chdir(path)
 
 data = UnityData('data/solo_161')
 pose_yaml_path = unity_to_yolo_pose(data, 'data/solo_161_yolo_pose', 5, [0, 2, 1, 4, 3], include_test=True)
