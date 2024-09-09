402c1d9308724ec392ae45ed65887ccfb3bdad80
Lukas Thiersch <lukas.thiersch@lansco.de>
2024-09-09 14:51:15 +0200

    Adapted shell scripts and configs

--- scripts/n_pose_vec/run.sh
+++ scripts/n_pose_vec/run.sh
@@ -6,6 +6,6 @@ export HTTP_PROXY=http://proxy:80
 export HTTPS_PROXY=http://proxy:80
 
 source .venv/bin/activate
-.venv/bin/python3 ./scripts/yolo/train_yolo.py
+.venv/bin/python3 ./scripts/train.py
 
 # salloc --gres=gpu:a100:1 -C a100_80 --partition=a100 --time=02:00:00
